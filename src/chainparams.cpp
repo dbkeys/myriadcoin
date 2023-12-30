37e06a3ad6c8a166f41b43de68eb34fc10925114
DBKeys <dbkeys@proton.me>
2023-12-30 10:40:34 +0000

    Add {seed,dnsseed}.myriadcoin.cc

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -197,6 +197,8 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
+        vSeeds.emplace_back("seed.myriadcoin.cc");
+        vSeeds.emplace_back("dnsseed.myriadcoin.cc");
         vSeeds.emplace_back("seed1.myriadcoin.org");
         vSeeds.emplace_back("seed2.myriadcoin.org");
         vSeeds.emplace_back("seed3.myriadcoin.org");
